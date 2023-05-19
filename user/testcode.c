5ffea2d2feaaeb4271af424306683719707ce127
rhit-martina9 <89958644+rhit-martina9@users.noreply.github.com>
2023-05-19 00:21:43 -0400

    Test suite, 4 tests

--- user/testcode.c
+++ user/testcode.c
@@ -9,27 +9,54 @@
 #include "kernel/riscv.h"
 #include "kernel/signal.h"
 
-void signal_handler() {
-    printf("Signal received.\n");
-}
+int pid;
 
-void simplesignal() {
-    for(int i = 0; i < 10; i++) {
-        printf("%d\n", i);
-        send_signal(SIGNAL_KILL,getpid(),&signal_handler);
+void killself() {
+    if(!(pid = fork())) {
+        send_signal(SIGNAL_KILL, getpid());
+        sleep(1);
+        printf("(killself) Self is alive\n");
+        exit(1);
+    } else {
+        wait(&pid);
     }
 }
 
+void killchild() {
+    if(!(pid = fork())) {
+        sleep(1);
+        printf("(killchild) Child is alive\n");
+        exit(1);
+    } else {
+        send_signal(SIGNAL_KILL, pid);
+    }
+}
+
+void whilekill() {
+    send_signal(SIGNAL_KILL, getpid());
+    while(1) {}
+    exit(1);
+}
+
+void simplekill() {
+    send_signal(SIGNAL_KILL, getpid());
+    sleep(2);
+    exit(1);
+}
+
 struct test {
-  void (*f)(char *);
-  char *s;
+    void (*f)(char *);
+    char *s;
 } quicktests[] = {
-    {simplesignal, "simplesignal"},
+    {simplekill, "simplekill"},
+    {whilekill, "whilekill"},
+    {killself, "killself"},
+    {killchild, "killchild"},
     { 0, 0},
 };
 
 struct test slowtests[] = {    
-  { 0, 0},
+    { 0, 0},
 };
 
 //
@@ -40,92 +67,95 @@ struct test slowtests[] = {
 // indicates success.
 int
 run(void f(char *), char *s) {
-  int pid;
-  int xstatus;
+    int pid;
+    int xstatus;
 
-  printf("test %s: ", s);
-  if((pid = fork()) < 0) {
-    printf("runtest: fork error\n");
-    exit(1);
-  }
-  if(pid == 0) {
-    f(s);
-    exit(0);
-  } else {
-    wait(&xstatus);
-    if(xstatus != 0) 
-      printf("FAILED\n");
-    else
-      printf("OK\n");
-    return xstatus == 0;
-  }
+    printf("test %s: ", s);
+    if((pid = fork()) < 0) {
+        printf("runtest: fork error\n");
+        exit(1);
+    }
+    if(pid == 0) {
+        f(s);
+        exit(0);
+    } else {
+        wait(&xstatus);
+        if(xstatus == 1) 
+        printf("FAILED\n");
+        else
+        printf("OK\n");
+        return xstatus != 1;
+    }
 }
 
 int
 runtests(struct test *tests, char *justone) {
-  for (struct test *t = tests; t->s != 0; t++) {
-    if((justone == 0) || strcmp(t->s, justone) == 0) {
-      if(!run(t->f, t->s)){
-        printf("SOME TESTS FAILED\n");
-        return 1;
-      }
+    for (struct test *t = tests; t->s != 0; t++) {
+        if((justone == 0) || strcmp(t->s, justone) == 0) {
+        if(!run(t->f, t->s)){
+            printf("SOME TESTS FAILED\n");
+            return 1;
+        }
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 
 int
 drivetests(int quick, int continuous, char *justone) {
-  do {
-    printf("usertests starting\n");
-    int free0 = countfree();
-    int free1 = 0;
-    if (runtests(quicktests, justone)) {
-      if(continuous != 2) {
-        return 1;
-      }
-    }
-    if(!quick) {
-      if (justone == 0)
-        printf("usertests slow tests starting\n");
-      if (runtests(slowtests, justone)) {
+    do {
+        printf("usertests starting\n");
+        if (runtests(quicktests, justone)) {
         if(continuous != 2) {
-          return 1;
+            return 1;
         }
-      }
-    }
-    if((free1 = countfree()) < free0) {
-      printf("FAILED -- lost some free pages %d (out of %d)\n", free1, free0);
-      if(continuous != 2) {
-        return 1;
-      }
-    }
-  } while(continuous);
-  return 0;
+        }
+        if(!quick) {
+        if (justone == 0)
+            printf("usertests slow tests starting\n");
+        if (runtests(slowtests, justone)) {
+            if(continuous != 2) {
+            return 1;
+            }
+        }
+        }
+    } while(continuous);
+    return 0;
 }
 
 int
 main(int argc, char *argv[])
 {
-  int continuous = 0;
-  int quick = 0;
-  char *justone = 0;
+    int continuous = 0;
+    int quick = 0;
+    char *justone = 0;
 
-  if(argc == 2 && strcmp(argv[1], "-q") == 0){
-    quick = 1;
-  } else if(argc == 2 && strcmp(argv[1], "-c") == 0){
-    continuous = 1;
-  } else if(argc == 2 && strcmp(argv[1], "-C") == 0){
-    continuous = 2;
-  } else if(argc == 2 && argv[1][0] != '-'){
-    justone = argv[1];
-  } else if(argc > 1){
-    printf("Usage: usertests [-c] [-C] [-q] [testname]\n");
-    exit(1);
-  }
-  if (drivetests(quick, continuous, justone)) {
-    exit(1);
-  }
-  printf("ALL TESTS PASSED\n");
-  exit(0);
+    if(argc == 2 && strcmp(argv[1], "-q") == 0){
+        quick = 1;
+    } else if(argc == 2 && strcmp(argv[1], "-c") == 0){
+        continuous = 1;
+    } else if(argc == 2 && strcmp(argv[1], "-C") == 0){
+        continuous = 2;
+    } else if(argc == 2 && argv[1][0] != '-'){
+        justone = argv[1];
+    } else if(argc > 1){
+        printf("Usage: usertests [-c] [-C] [-q] [testname]\n");
+        exit(1);
+    }
+    if (drivetests(quick, continuous, justone)) {
+        exit(1);
+    }
+
+    // printf("usertests starting\n");
+    // printf("running simplekill\n");
+    // simplekill();
+    // printf("running whilekill\n");
+    // whilekill();
+    // printf("running killself\n");
+    // killself();
+    // printf("running killchild\n");
+    // killchild();
+
+    printf("ALL TESTS PASSED\n");
+    exit(0);
 }
