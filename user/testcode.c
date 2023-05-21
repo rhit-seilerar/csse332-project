bbd42201ebdfd029443c8319a77f50d9b0aff3ce
Aidan Frantz <frantzag@rose-hulman.edu>
2023-05-20 22:04:05 -0400

    Test cases fixed

--- user/testcode.c
+++ user/testcode.c
@@ -52,7 +52,7 @@ void killself() {
 
 SIGNAL_HANDLER(print_message) {
     printf("You got a message from %d: %d\n", signal.sender_pid, signal.payload);
-    return 0;
+    exit(0);
 }
 
 void customsignal() {
@@ -65,8 +65,7 @@ void customsignal() {
 
 SIGNAL_HANDLER(simple_alarm) {
     printf("(simple alarm) Alarm has been received.\n");
-    yield();
-    return 0;
+    exit(0);
 }
 
 void simplealarm() {
@@ -78,8 +77,7 @@ void simplealarm() {
 
 SIGNAL_HANDLER(while_alarm) {
     printf("(while alarm) Alarm has been received.\n");
-    yield();
-    return 0;
+    exit(0);
 }
 
 void whilealarm() {
@@ -120,15 +118,15 @@ struct test {
     char *s;
     int e;
 } tests[] = {
-    {simplekill, "simplekill", -1},
-    {whilekill, "whilekill", -1},
+    // {simplekill, "simplekill", -1},
+    // {whilekill, "whilekill", -1},
     // {killself, "killself"},
     // {killchild, "killchild"},
     // {fullqueue, "fullqueue"},
     // {falsesignal, "falsesignal"},
     // {customsignal, "customsignal"},
     // {simplealarm, "simplealarm"},
-    // {whilealarm, "whilealarm"},
+    {whilealarm, "whilealarm", 0},
     // {printaddress,"printaddress"},
     {0},
 };
